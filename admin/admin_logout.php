2835ec1bc38df6699af69b92139cda731d2ef43f
Napha-a1 <67319100045@chetupon.ac.th>
2025-08-28 06:33:07 +0700

    Update admin_logout.php

--- admin/admin_logout.php
+++ admin/admin_logout.php
@@ -1,5 +1,21 @@
 <?php
 session_start();
+// ล้างตัวแปร session ทั้งหมด
+$_SESSION = array();
+
+// ลบ session cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// ทำลาย session
 session_destroy();
-header("Location: admin_login.php");
+
+// Redirect ไปหน้า Admin Login
+header("Location: login.php");
 exit();
+?>
